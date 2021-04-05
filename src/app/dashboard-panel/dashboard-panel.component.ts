39491163057cb3d850b38f201ed412a83bf61165
Mark Mulvaney <mark@bambwa.com>
2021-04-05 19:21:40 +0200

    restructuring

--- src/app/dashboard-panel/dashboard-panel.component.ts
+++ src/app/dashboard-panel/dashboard-panel.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { HOUSE } from '../house';
 import { HouseDataService } from '../house-data.service';
 
 @Component({
@@ -8,25 +7,7 @@ import { HouseDataService } from '../house-data.service';
   styleUrls: ['./dashboard-panel.component.scss'],
 })
 export class DashboardPanelComponent implements OnInit {
-  house = HOUSE;
-  myRoom: object;
-  roomTitle: string;
-  data: number;
-  title: string;
-  constructor(private houseDataService: HouseDataService) {
-    this.data = 0;
-    this.myRoom = this.house[0];
-    this.roomTitle = this.house[0].title;
-    this.title = '';
-    this.panel = '';
-    this.overview = false;
-    this.roomsList = false;
-    this.roomDetail = false;
-  }
-  @Input() panel: string;
-  @Input() overview: boolean;
-  @Input() roomsList: boolean;
-  @Input() roomDetail: boolean;
+  constructor(private houseDataService: HouseDataService) {}
 
   ngOnInit(): void {}
 }
